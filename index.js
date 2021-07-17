342f70ceb3373b9dcab64fab50d7327acdc44690
Persorkki <persorkki@protonmail.com>
2021-07-17 19:59:09 +0300

    3.5&3.6 puhelinluettelon backend step5&step6

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 phoneNumbers = [
     {
         id: 1,
@@ -52,6 +54,44 @@ app.delete('/api/persons/:id', (req, res) => {
     else {
         res.status(400).end()
     }
+})
+
+/*  generoidaan satunnainen ID ja katsotaan löytyykö se jo phonebookista
+    jos löytyy, niin tehdään uusia kunnes löytyy sellainen joka ei ole käytössä
+    ja kyllä, jää jumiin jos kaikki numerot viety*/
+const getRandomId = () => {
+    let id = 0
+    do {
+        id = Math.floor(Math.random() * 100000)
+    } while (phoneNumbers.find(pn => pn.id === id))
+    return id
+}
+
+
+app.post('/api/persons/', (req, res) => {
+    const body = req.body
+
+    //lähetetään virhe jos nimi tai numero puuttuu
+    if (!body.name || !body.number) {
+        res.status(400).json({error:`missing name or number`})
+    }
+    else {
+        //jos annettua nimeä ei löydy puhelinluettelosta, voidaan lisätä se listaan
+        if (!phoneNumbers.find(pn => pn.name === body.name)) {
+            const newObj = {
+                id: getRandomId(),
+                name: body.name,
+                number: body.number,
+            }
+            phoneNumbers = phoneNumbers.concat(newObj)
+            res.json(newObj)
+        }
+        else {
+            //ja jos nimi löytyi jo puhelinluettelosta, annetaan virhe
+            res.status(400).json({error:`name is already in the phonebook`})
+        }
+    }
+    
 })
 
 app.get('/info', (req, res) => {
