95e3cbadaf8582f6ca2f48fa937f7cb1dab046d0
Persorkki <persorkki@protonmail.com>
2021-07-19 01:24:58 +0300

    added frontend

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const app = express()
 
-app.use(express.json())
+const app = express()
 
 morgan.token('body', (req, res) => {
     return JSON.stringify(req.body)
 })
+
+app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
-
+app.use(express.static('build'))
 
 phoneNumbers = [
     {
